68bdc7c174219cf7b6a3c0328d3b4fdf981d85c9
Goroll, Torsten (GORTOR) <torsten.goroll@eos.info>
2026-03-26 16:26:47 +0000

    now exporting method helper

--- packages/client/src/callMethodArgument.ts
+++ packages/client/src/callMethodArgument.ts
@@ -1,6 +1,6 @@
 import { DataValue, DiagnosticInfo, ExpandedNodeId, ExtensionObject, LocalizedText, NodeId, QualifiedName, UaPrimitive, Variant, XmlElement } from "opcjs-base";
 
-export type MethodArgument =
+export type CallMethodArgument =
   | UaPrimitive
   | NodeId
   | ExpandedNodeId
